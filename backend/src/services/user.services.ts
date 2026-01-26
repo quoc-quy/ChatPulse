63a5cd518ad6021e4d54b76b63f084f48f970123
EricMai2112 <mait58674@gmail.com>
2026-01-26 15:30:50 +0700

    receiving accesstoken and refreshtoken when registration successfully

--- backend/src/services/user.services.ts
+++ backend/src/services/user.services.ts
@@ -1,17 +1,49 @@
 import User from '~/models/schemas/user.schema'
 import databaseService from './database.services'
 import { RegisterReqBody } from '~/models/requests/users.requests'
+import { hashPassword } from '~/utils/crypto'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enum'
 
 class UserService {
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.AccessToken
+      },
+      options: {
+        expiresIn: '1h'
+      }
+    })
+  }
+
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.RefreshToken
+      },
+      options: {
+        expiresIn: '100d'
+      }
+    })
+  }
+
   async register(payload: RegisterReqBody) {
-    const { email, password, userName, date_of_birth, phone } = payload
     const result = await databaseService.users.insertOne(
       new User({
         ...payload,
-        date_of_birth: new Date(date_of_birth)
+        date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password)
       })
     )
-    return result
+    const user_id = result.insertedId.toString()
+    const [access_token, refresh_token] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ])
+    return { access_token, refresh_token }
   }
 
   async checkExistedEmail(email: string) {
