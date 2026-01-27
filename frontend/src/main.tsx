633157f3bf966f3f2fa512ceb0446f6c8e4a0d50
quoc-quy <quyleom10@gmail.com>
2026-01-27 18:38:32 +0700

    feat: update dependencies and add chat/auth pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,27 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster richColors />
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
-);
+  </StrictMode>
+)
--- frontend/src/useRouteElements.tsx
+++ frontend/src/useRouteElements.tsx
@@ -1,18 +1,41 @@
-import { useRoutes } from "react-router-dom";
-import MainLayout from "./layouts";
-import HomeScreen from "./screens/HomeScreen";
+import { useRoutes } from 'react-router-dom'
+import MainLayout from './layouts'
+import ChatAppPage from './pages/ChatAppPage'
+import SignUpPage from './pages/SignUpPage'
+import SignInPage from './pages/SignInPage'
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
+    // private routes
     {
-      path: "/",
+      path: '/',
       index: true,
       element: (
         <MainLayout>
-          <HomeScreen />
+          <ChatAppPage />
         </MainLayout>
-      ),
+      )
     },
-  ]);
-  return routeElements;
+
+    // public routes
+    {
+      path: '/signup',
+      index: true,
+      element: (
+        <MainLayout>
+          <SignUpPage />
+        </MainLayout>
+      )
+    },
+    {
+      path: '/signin',
+      index: true,
+      element: (
+        <MainLayout>
+          <SignInPage />
+        </MainLayout>
+      )
+    }
+  ])
+  return routeElements
 }
