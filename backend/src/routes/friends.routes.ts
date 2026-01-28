1d5bb56b059506553100f51887144b98a987a494
snowyy16 <huynhson532004@gmail.com>
2026-01-28 18:21:19 +0700

    fix: friend routes

---
+++ backend/src/routes/friends.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express'
+import {
+  acceptFriendRequestController,
+  createFriendRequestController,
+  getFriendListController,
+  getReceivedFriendRequestsController
+} from '~/controllers/friends.controllers'
+import { accessTokenValidator } from '~/middlewares/users.middlewares'
+import { wrapRequestHandler } from '~/utils/handlers'
+
+const friendsRouter = Router()
+
+/**
+ * Gửi lời mời kết bạn
+ * Path: POST /friends/request
+ */
+friendsRouter.post('/request', accessTokenValidator, wrapRequestHandler(createFriendRequestController))
+
+/**
+ * Chấp nhận lời mời kết bạn
+ * Path: POST /friends/accept
+ */
+friendsRouter.post('/accept', accessTokenValidator, wrapRequestHandler(acceptFriendRequestController))
+
+/**
+ * Lấy danh sách bạn bè
+ * Path: GET /friends/list
+ */
+friendsRouter.get('/list', accessTokenValidator, wrapRequestHandler(getFriendListController))
+
+/**
+ * Lấy danh sách lời mời đã nhận
+ * Path: GET /friends/requests/received
+ */
+friendsRouter.get('/requests/received', accessTokenValidator, wrapRequestHandler(getReceivedFriendRequestsController))
+
+export default friendsRouter
