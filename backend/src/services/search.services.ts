0eac3229dbf67a6fa5d44cdc70db633e204921cf
Binard2112 <mait58674@gmail.com>
2026-01-31 16:40:29 +0700

    feat: Handle search by userName and phone

--- backend/src/services/search.services.ts
+++ backend/src/services/search.services.ts
@@ -1,6 +1,73 @@
+import { ObjectId } from 'mongodb'
+import databaseService from './database.services'
+
 class SearchService {
-  async search({}) {
-    
+  async search({
+    user_id,
+    userName,
+    phone,
+    limit,
+    page
+  }: {
+    user_id: string
+    userName: string
+    phone: string
+    limit: number
+    page: number
+  }) {
+    const $match: any = {}
+
+    if (phone) {
+      $match.phone = phone
+    }
+
+    if (userName) {
+      const followed_user_id = await databaseService.friends
+        .find(
+          {
+            user_id: new ObjectId(user_id)
+          },
+          {
+            projection: {
+              friend_id: 1
+            }
+          }
+        )
+        .toArray()
+
+      const idfriendIds = followed_user_id.map((follower) => follower.friend_id)
+
+      $match._id = {
+        $in: idfriendIds
+      }
+      $match.userName = {
+        $regex: userName,
+        $options: 'i' //Khong phan biet hoa thuong
+      }
+    }
+
+    const result = await databaseService.users
+      .aggregate([
+        {
+          $match
+        },
+        {
+          $project: {
+            password: 0,
+            created_at: 0,
+            updated_at: 0
+          }
+        },
+        {
+          $skip: limit * (page - 1)
+        },
+        {
+          $limit: limit
+        }
+      ])
+      .toArray()
+
+    return result
   }
 }
 
