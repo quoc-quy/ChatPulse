12da235adb09c7455b5f40274b99cb272ffc3289
quoc-quy <quyleom10@gmail.com>
2026-02-28 20:18:56 +0700

    feat(ui): implement dark/light/system mode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-import useRouteElements from "./useRouteElements";
+import { ThemeProvider } from './components/theme-provider'
+import useRouteElements from './useRouteElements'
 
 function App() {
-  const routeElements = useRouteElements();
-  return <div>{routeElements}</div>;
+  const routeElements = useRouteElements()
+  return (
+    <ThemeProvider defaultTheme='system' storageKey='chatpulse-theme'>
+      {routeElements}
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
