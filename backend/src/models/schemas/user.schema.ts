2e770e547ba6382242c062f98a763c1b2d30d580
EricMai2112 <mait58674@gmail.com>
2026-03-03 00:04:40 +0700

    update(user): add more attributes for user schema

--- backend/src/models/schemas/user.schema.ts
+++ backend/src/models/schemas/user.schema.ts
@@ -6,9 +6,13 @@ interface UserType {
   password: string
   date_of_birth: Date
   email: string
+  phone: string
   avatar?: string
   bio?: string
-  phone: string
+  gender?: string
+  forgot_password_token?: string
+  last_active_at?: Date
+  fcm_token?: string
   created_at?: Date
   updated_at?: Date
 }
@@ -22,10 +26,29 @@ export default class User {
   avatar: string
   bio: string
   phone: string
+  gender: string
+  forgot_password_token: string
+  last_active_at: Date
+  fcm_token: string
   created_at: Date
   updated_at: Date
 
-  constructor({ _id, userName, password, date_of_birth, email, avatar, bio, phone, created_at, updated_at }: UserType) {
+  constructor({
+    _id,
+    userName,
+    password,
+    date_of_birth,
+    email,
+    avatar,
+    bio,
+    phone,
+    gender,
+    forgot_password_token,
+    last_active_at,
+    fcm_token,
+    created_at,
+    updated_at
+  }: UserType) {
     const date = new Date()
     this._id = _id
     this.userName = userName
@@ -35,6 +58,10 @@ export default class User {
     this.avatar = avatar || ''
     this.bio = bio || ''
     this.phone = phone || ''
+    this.gender = gender || ''
+    this.forgot_password_token = forgot_password_token || ''
+    this.last_active_at = last_active_at || date
+    this.fcm_token = fcm_token || ''
     this.created_at = created_at || date
     this.updated_at = updated_at || date
   }
