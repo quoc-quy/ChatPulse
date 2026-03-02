804d66935ae11d57611afe802743ff59a3ef3043
EricMai2112 <mait58674@gmail.com>
2026-03-03 00:28:13 +0700

    fix(routes): handle auth routes for login and register

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -21,25 +21,6 @@ import { wrapRequestHandler } from '~/utils/handlers'
 const usersRouter = Router()
 
 /**
- * Login account
- * Body: {email: string, password: string}
- */
-usersRouter.post('/login', loginValidator, wrapRequestHandler(loginController))
-
-/**
- * Register a new user
- * Body: {email: string, password: string, confirm_password: string, userName: string, date_of_birth: ISOString, phone: number}
- */
-usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
-
-/**
- * Logout
- * Header: {Authorization: Bearer <access_token>}
- * body: {refresh_token: string}
- */
-usersRouter.post('/logout', accessTokenValidator, refreshTokenValidator, wrapRequestHandler(logoutController))
-
-/**
  * Get me
  * Header: {Authorization: Bearer <access_token>}
  */
