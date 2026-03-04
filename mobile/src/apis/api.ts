0f612e3dfb9bce114a644344915bad675d044db5
snowyy16 <huynhson532004@gmail.com>
2026-03-04 21:28:42 +0700

    feat(api): add API module for handling user and conversation requests

---
+++ mobile/src/apis/api.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios"
+
+export const api = axios.create({
+  baseURL: "http://192.168.x.x:4000"
+})
+// Thêm interceptor để tự động gắn Token
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
