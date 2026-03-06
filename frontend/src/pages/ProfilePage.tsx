e91fb2aa42f1a8ec13b634b4007b13c1bb8e5da2
EricMai2112 <mait58674@gmail.com>
2026-03-07 01:17:42 +0700

    feat(profile): handle display user information when login successfully

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,8 +1,35 @@
+import userApi from '@/apis/user.api'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function ProfilePage() {
+  const { data: userData } = useQuery({
+    queryKey: ['profile'],
+    queryFn: userApi.getMe
+  })
+
+  const { register, reset, setValue, watch } = useForm()
+
+  const profile = userData?.data.user
+
+  useEffect(() => {
+    if (profile) {
+      reset({
+        userName: profile.userName,
+        email: profile.email,
+        date_of_birth: profile.date_of_birth?.slice(0, 10),
+        phone: profile.phone,
+        gender: profile.gender,
+        bio: profile.bio
+      })
+    }
+  }, [profile, reset])
+
   return (
     <div className='flex flex-1 flex-col bg-muted/10 p-8'>
       <h2 className='text-2xl font-semibold mb-8 text-center text-foreground'>Hồ sơ của tôi</h2>
@@ -10,7 +37,7 @@ export default function ProfilePage() {
       {/* Avatar */}
       <div className='flex flex-col items-center mb-8 gap-4'>
         <div className='h-28 w-28 overflow-hidden text-foreground rounded-full border'>
-          <img alt='avatar' className='h-full w-full object-cover' />
+          <img src={profile?.avatar} alt='avatar' className='h-full w-full object-cover' />
         </div>
       </div>
 
@@ -19,39 +46,49 @@ export default function ProfilePage() {
         <div className='grid grid-cols-2 gap-6'>
           <div>
             <Label className='text-sm font-medium text-foreground'>User Name</Label>
-            <Input name='userName' />
+            <Input {...register('userName')} />
           </div>
 
           <div>
             <Label className='text-sm font-medium text-foreground'>Email</Label>
-            <Input name='email' type='email' />
-          </div>
-
-          <div>
-            <Label className='text-sm font-medium text-foreground'>Password</Label>
-            <Input name='password' type='password' />
+            <Input {...register('email')} type='email' />
           </div>
 
           <div>
             <Label className='text-sm font-medium text-foreground'>Date of Birth</Label>
-            <Input name='date_of_birth' type='date' />
+            <Input {...register('date_of_birth')} type='date' />
           </div>
 
           <div>
             <Label className='text-sm font-medium text-foreground'>Phone</Label>
-            <Input name='phone' />
+            <Input {...register('phone')} />
           </div>
 
           <div>
             <Label className='text-sm font-medium text-foreground'>Gender</Label>
-            <Input name='gender' placeholder='male / female' />
+
+            <RadioGroup
+              value={watch('gender')}
+              className='flex gap-6 mt-2'
+              onValueChange={(value) => setValue('gender', value)}
+            >
+              <div className='flex text-foreground items-center space-x-2'>
+                <RadioGroupItem value='male' id='male' />
+                <Label htmlFor='male'>Male</Label>
+              </div>
+
+              <div className='flex text-foreground items-center space-x-2'>
+                <RadioGroupItem value='female' id='female' />
+                <Label htmlFor='female'>Female</Label>
+              </div>
+            </RadioGroup>
           </div>
 
           {/* bio full row */}
           <div className='col-span-2'>
             <Label className='text-sm font-medium text-foreground'>Bio</Label>
             <textarea
-              name='bio'
+              {...register('bio')}
               placeholder='Nhập giới thiệu bản thân...'
               className='w-full mt-2 min-h-[120px] shadow-xs text-foreground rounded-md border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ring'
             />
